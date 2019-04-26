ea929c17ca320a30b1743803cb4093369b58cb48
akicho8 <akicho8@gmail.com>
2019-04-26 14:21:15 +0900

    [chore] Code that reproduces the behavior of b-table

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,69 @@
-<template>
-  <section class="section">
-    <div class="columns is-mobile">
+<template lang="pug">
+.section
+  b-table(
+    :data="data"
 
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub</a>
-      </card>
+    backend-sorting
+    :default-sort="[sortField, sortOrder]"
+    @sort="onSort"
+    )
 
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">Every</b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">Vue.js</a> and <a href="http://bulma.io/">Bulma</a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-
-    </div>
-  </section>
+    template(slot-scope="props")
+      b-table-column(field="title"      label="Title" sortable v-text="props.row.title")
+      b-table-column(field="vote_count" label="Vote"  sortable v-text="props.row.vote_count")
+      b-table-column(label="Operation") show, edit
 </template>
 
 <script>
-import Card from '~/components/Card'
+import axios from "axios"
 
 export default {
   name: 'HomePage',
 
-  components: {
-    Card
-  }
+  data() {
+    return {
+      data: [],
+      total: 0,
+      sortField: "vote_count",
+      sortOrder: "desc",
+      page: 1,
+      perPage: 20,
+    }
+  },
+
+  methods: {
+    onSort(field, order) {
+      this.sortField = field
+      this.sortOrder = order
+      this.loadAsyncData()
+    },
+
+    loadAsyncData() {
+      const params = [
+        'api_key=bb6f51bef07465653c3e553d6ab161a8',
+        'language=en-US',
+        'include_adult=false',
+        'include_video=false',
+        `sort_by=${this.sortField}.${this.sortOrder}`,
+        `page=${this.page}`
+      ].join('&')
+
+      axios.get(`https://api.themoviedb.org/3/discover/movie?${params}`)
+        .then(({ data }) => {
+          this.data = []
+          this.total = data.total_results
+          data.results.forEach(item => this.data.push(item))
+        })
+        .catch((error) => {
+          this.data = []
+          this.total = 0
+          throw error
+        })
+    },
+  },
+
+  mounted() {
+    this.loadAsyncData()
+  },
 }
 </script>
